715be23ace67d958f10d0eb6d841817cb34b3994
Luqman Bello <63483816+MateerialRepo@users.noreply.github.com>
2021-03-18 14:45:58 +0100

    updated code with refactoreed

--- disemvowelTrolls.py
+++ disemvowelTrolls.py
@@ -1,8 +1,14 @@
 def disemvowel(string):
     check = ['a','A','e','E','i','I','o','O','u','U']
     for i in check:
-      if i =='a' or i=='A':
+      if i =='a' :
         answ = string.replace(i,'')
       else:
         answ = answ.replace(i,'')
     return answ
+
+#refactored implementation
+# def disemvowel(s):
+#     for i in "aeiouAEIOU":
+#         s = s.replace(i,'')
+#     return s
